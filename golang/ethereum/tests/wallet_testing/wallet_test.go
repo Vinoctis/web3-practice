201b92cbfd987ddffa06e2f58357686e34929ac5
vinoctis <vinsonte20080808@live.cn>
2025-06-25 22:42:12 +0800

    .

--- golang/ethereum/tests/wallet_testing/wallet_test.go
+++ golang/ethereum/tests/wallet_testing/wallet_test.go
@@ -5,7 +5,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/Vinoctis/ethereum-practice/wallet"
 	"github.com/Vinoctis/ethereum-practice/svc"
-	"math/big"
+	// "math/big"
 )
 
 // func TestCrateWllet(t *testing.T) {
@@ -19,12 +19,12 @@ func TestTransfer(t *testing.T) {
 	assert.NotNil(t, privateKey)
 	ethClient := svc.NewEthClient()
 	assert.NotNil(t, ethClient)
-	toAddress := "0x3f8fBE733456e503c689451c4953E7B680B668d1"
-	value := new(big.Int)
-	value.SetString("100000000000000", 10) //10^-4 eth 
-	transactionHash := wallet.Transfer(ethClient, privateKey, toAddress, value)
-	assert.True(t, len(transactionHash) == 66 , "交易的 hash 不合法")
-	assert.True(t, transactionHash[:2] == "0x", "交易哈希缺少前缀0x")
+	// toAddress := "0x3f8fBE733456e503c689451c4953E7B680B668d1"
+	// value := new(big.Int)
+	// value.SetString("100000000000000", 10) //10^-4 eth 
+	// transactionHash := wallet.Transfer(ethClient, privateKey, toAddress, value)
+	// assert.True(t, len(transactionHash) == 66 , "交易的 hash 不合法")
+	// assert.True(t, transactionHash[:2] == "0x", "交易哈希缺少前缀0x")
 }
 
 
