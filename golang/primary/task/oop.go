2a304063923256512d7b6d014379a65c52d48077
fei_pan <fei_pan@intsig.net>
2025-07-15 15:12:12 +0800

    feat:组合结构体

--- golang/primary/task/oop.go
+++ golang/primary/task/oop.go
@@ -51,8 +51,31 @@ func (c Circle) Perimeter() float64 {
 	return 2 * 3.14 * c.Radius
 }
 
-// 题目 ：使用组合的方式创建一个 Person 结构体，包含 Name 和 Age 字段，再创建一个 Employee 结构体，组合 Person 结构体并添加 EmployeeID 字段。为 Employee 结构体实现一个 PrintInfo() 方法，输出员工的信息。
+// 题目 ：使用组合的方式创建一个 Person 结构体，包含 Name 和 Age 字段，
+// 再创建一个 Employee 结构体，组合 Person 结构体并添加 EmployeeID 字段。为 Employee 结构体实现一个 PrintInfo() 方法，输出员工的信息。
 // 考察点 ：组合的使用、方法接收者。
+type Person struct {
+	Name string 
+	Age int 
+}
+
+type Employee struct {
+	EmployeeID int
+	Person
+}
+
+func NewEmployee(id int, p Person) Employee {
+	return Employee{
+		EmployeeID: id,
+		Person: p,
+	}
+}
+
+func (e Employee) PrintInfo(id int) {
+	fmt.Println("员工姓名：", e.Name)
+	fmt.Println("员工年龄：", e.Age)
+	fmt.Println("员工ID：", e.EmployeeID)
+}
 
 
 func main() {
@@ -63,4 +86,11 @@ func main() {
 	circle := NewCircle(5.5)
 	fmt.Println("圆面积：", circle.Area())
 	fmt.Println("圆周长：", circle.Perimeter())
+
+	p:= Person{
+		Name: "Vinoctis",
+		Age: 18,
+	}
+	e:= NewEmployee(1, p)
+	e.PrintInfo(1)
 }
