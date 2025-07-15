941ad4e614acde1be594327754b09cbec12d09a8
fei_pan <fei_pan@intsig.net>
2025-07-15 09:30:38 +0800

    feat:添加注释

--- golang/primary/task/common_prefix.go
+++ golang/primary/task/common_prefix.go
@@ -6,9 +6,11 @@ func CommonPrefix(s []string) string {
 	if len(s) == 0 {
 		return ""
 	}
-
+	// 遍历首字符串的字符
 	for i:=0;i<len(s[0]);i++ {
+		//遍历其他字符串的字符
 		for j:=1;j<len(s);j++ {
+			//如果长度超出字符串长度，或出现不相同的情况直接返回首字符串当前遍历索引，就是公共前缀
 			if i >= len(s[j]) - 1  || s[j][i] != s[0][i] {
 				return s[0][:i]
 			}
