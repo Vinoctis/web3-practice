cd4722b3bda23ba5422f350633db59ed4fe7b19d
fei_pan <fei_pan@intsig.net>
2025-07-15 15:05:17 +0800

    feat:矩形和圆形，oop实现接口的方法面积和周长

---
+++ golang/primary/task/oop.go
@@ -0,0 +1,66 @@
+package main 
+import(
+	"fmt"
+)
+
+// 题目 ：定义一个 Shape 接口，包含 Area() 和 Perimeter() 两个方法。
+// 然后创建 Rectangle 和 Circle 结构体，实现 Shape 接口。
+// 在主函数中，创建这两个结构体的实例，并调用它们的 Area() 和 Perimeter() 方法。
+// 考察点 ：接口的定义与实现、面向对象编程风格。
+type Shape interface{
+	Area() float64
+	Perimeter() float64
+}
+
+type Rectangle struct {
+	Width 	float64
+	Height 	float64
+}
+
+func NewRectangle(w, h float64) Rectangle{
+	return Rectangle{
+		Width: w,
+		Height: h,
+	}
+}
+
+func(r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Height)
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func NewCircle(r float64) Circle{
+	return Circle{
+		Radius: r,
+	}
+}
+
+
+func (c Circle) Area() float64 {
+	return 3.14 * c.Radius * c.Radius
+}
+
+func (c Circle) Perimeter() float64 {
+	return 2 * 3.14 * c.Radius
+}
+
+// 题目 ：使用组合的方式创建一个 Person 结构体，包含 Name 和 Age 字段，再创建一个 Employee 结构体，组合 Person 结构体并添加 EmployeeID 字段。为 Employee 结构体实现一个 PrintInfo() 方法，输出员工的信息。
+// 考察点 ：组合的使用、方法接收者。
+
+
+func main() {
+	rectangle := NewRectangle(10, 20)
+	fmt.Println("矩形面积：", rectangle.Area())
+	fmt.Println("矩形周长：", rectangle.Perimeter())
+
+	circle := NewCircle(5.5)
+	fmt.Println("圆面积：", circle.Area())
+	fmt.Println("圆周长：", circle.Perimeter())
+}
