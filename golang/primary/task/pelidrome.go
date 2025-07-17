39853b86289f2d1707ceed87eb282f02eed3f36e
fei_pan <fei_pan@intsig.net>
2025-07-17 14:05:36 +0800

    feat: 判断回文方法新增数学方法

--- golang/primary/task/pelidrome.go
+++ golang/primary/task/pelidrome.go
@@ -5,23 +5,45 @@ import (
 	"fmt"
 
 )
-func isPalindrome(num int) {
+func isPalindrome(num int) bool {
 
 	str := strconv.Itoa(num)
 	runes := []rune(str)
 	for i, j := 0, len(str) - 1 ; i < j;i,j = i+1, j-1 {
 		runes[i], runes[j] = runes[j], runes[i]
 	}
-	if str == string(runes) {
-		fmt.Println(num,"是回文数")
-	} else {
+	return str == string(runes)
+}
+
+func isPalindromeByNumeric(num int) bool {
+	// 0 和能被10整除都不是回文数
+	if num == 0 || (num%10 == 0 && num != 0) {
 		fmt.Println(num,"不是回文数")
 	}
+
+	reserve := 0 
+	for num > reserve {
+		reserve = reserve * 10 + num%10
+		num /= 10 
+	}
+	
+	return num == reserve || num == reserve/10
 }
 
 func main() {
-	isPalindrome(123321)
-	isPalindrome(123)
-	isPalindrome(45654)
-	isPalindrome(1)
+	nums := []int{12321, 123, 45654, 1}
+	for _, v := range nums {
+		//字符串方法
+		if isPalindrome(v) {
+			fmt.Println(v,"是回文数")
+		} else {
+			fmt.Println(v,"不是回文数")
+		}
+		//数组方法
+		if isPalindromeByNumeric(v) {
+			fmt.Println(v,"是回文数")
+		} else {
+			fmt.Println(v,"不是回文数")
+		}
+	}
 }
